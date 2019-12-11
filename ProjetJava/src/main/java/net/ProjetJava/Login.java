ce7d1c93ec4fac8479a3fb13069b4cb5dd7689a7
Erayana <fannybaulu35@gmail.com>
2019-12-11 10:59:09 +0100

    Maquettes, FXML, Cahier

---
+++ ProjetJava/src/main/java/net/ProjetJava/Login.java
@@ -0,0 +1,21 @@
+package net.ProjetJava;
+
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+public class Login {
+
+	public static void main(String[] args) {
+		System.setProperty("java.security.auth.login.config", "jaas.config");
+
+		String name = "myName";
+		String password = "myPassword";
+
+		try {
+			LoginContext lc = new LoginContext("Test", new TestCallbackHandler(name, password));
+			lc.login();
+		} catch (LoginException e) {
+			e.printStackTrace();
+		}
+	}
+}
