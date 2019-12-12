393aeeca54d1adfa68d19296a99cb379a376f0c6
Erayana <fannybaulu35@gmail.com>
2019-12-12 19:26:38 +0100

    LogIn

--- ProjetJava/src/main/java/net/ProjetJava/LogIn.java
+++ ProjetJava/src/main/java/net/ProjetJava/LogIn.java
@@ -1,6 +1,9 @@
 package net.ProjetJava;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,10 +27,29 @@ private Button login;
 @FXML
 private Label create;
 
+public boolean checkLogin() {
+
+	if (!TFusername.getText().equals("") && !TFpassword.getText().equals("")) {
+		ResultSet test = Database.getInstance().query("SELECT * FROM utilisateur WHERE UtilisateurNom = '"+TFusername.getText()+"'");
+		try {
+			while (test.next()) {
+				if (test.getString(3).equals(TFpassword.getText())) {
+					return true;
+				}
+			}
+			return false;
+
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	return false;
+}
 
 
 public void Login (ActionEvent event) throws IOException {
-	if (TFusername.getText().equals("user")&& TFpassword.getText().contentEquals("pass")) {
+	Boolean test = checkLogin();
+	if (test == true) {
 		lblstatus.setText("Login Success");
 		App.setRoot("EquipSel");
 	}
